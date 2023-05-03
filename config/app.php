bac862487508bb8f5f75a82d527713baf0b137d0
RuggeriRino <rino.ruggeri@gmail.com>
2023-05-03 17:40:32 +0200

    add-config

--- config/app.php
+++ config/app.php
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Elibyy\TCPDF\ServiceProvider::class,
 
     ],
 
@@ -211,5 +212,6 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
+   'PDF' => Elibyy\TCPDF\Facades\TCPDF::class
 
 ];
